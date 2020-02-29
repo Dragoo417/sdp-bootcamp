235be5b15739ce132086fa94012b4091004fbfb0
Dorian <doriansporefan@gmail.com>
2020-02-29 14:35:03 +0100

    first stupid app

--- app/src/main/java/com/example/bootcamp/MainActivity.java
+++ app/src/main/java/com/example/bootcamp/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.bootcamp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.bootcamp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View v){
+        Intent intent = new Intent(this, GreetingActivity.class);
+        EditText editText = (EditText) findViewById(R.id.mainText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
